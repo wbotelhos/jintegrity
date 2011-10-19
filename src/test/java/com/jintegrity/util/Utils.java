788dfdae3fd5a3860cd22c519b7982f5d7f63787
Washington Botelho <wbotelhos@gmail.com>
2011-10-19 20:48:54 -0200

    Separeted sql functions on utils file.

--- src/test/java/com/jintegrity/util/Utils.java
+++ src/test/java/com/jintegrity/util/Utils.java
@@ -1,5 +1,6 @@
 package com.jintegrity.util;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -8,11 +9,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.jintegrity.core.DbUnitManager;
+import com.jintegrity.helper.SQLHelper;
+import com.jintegrity.model.Contact;
 import com.jintegrity.model.User;
 
 public class Utils {
 
-	public static List<User> loadAll(DbUnitManager dbUnitManager) throws SQLException {
+	private static final SQLHelper sqlHelper = new SQLHelper();
+
+	public static void insertAll() throws SQLException, IOException {
+		sqlHelper.run("sql/drop-contact", "sql/drop-user", "sql/create-user", "sql/create-contact", "sql/insert-user", "sql/insert-contact");
+	}
+
+	public static void createAll() throws SQLException, IOException {
+		sqlHelper.run("sql/create-user", "sql/create-contact");
+	}
+
+	public static void dropAll() throws SQLException, IOException {
+		sqlHelper.run("sql/drop-contact", "sql/drop-user");
+	}
+
+	public static List<User> loadAllUsers(DbUnitManager dbUnitManager) throws SQLException {
 		Connection conn = dbUnitManager.getConnection();
 		PreparedStatement ps = conn.prepareStatement("SELECT id, email, name FROM User");
 		List<User> userList = new ArrayList<User>();
@@ -29,6 +46,25 @@ public class Utils {
 		}
 
 		return userList;
+	}
+
+	public static List<Contact> loadAllContacts(DbUnitManager dbUnitManager) throws SQLException {
+		Connection conn = dbUnitManager.getConnection();
+		PreparedStatement ps = conn.prepareStatement("SELECT id, phone FROM Contact");
+		List<Contact> contactList = new ArrayList<Contact>();
+
+		try {
+			ResultSet rs = ps.executeQuery();
+
+			while (rs.next()) {
+				contactList.add(new Contact(rs.getLong("id"), rs.getString("phone")));
+			}
+		} finally {
+			conn.close();
+			ps.close();
+		}
+
+		return contactList;
 	}
 
 }
